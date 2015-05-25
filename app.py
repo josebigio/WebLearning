bb57bbc0cc82a3c48d0227945829ad2de251657d
jose Bigio <sirownsalot@gmail.com>
2015-05-25 10:58:48 -0500

    set up db

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 import os
 
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+db = SQLAlchemy(app)
+
+from models import Result
+
+#sanity checks
 print(os.environ['APP_SETTINGS'])
 
 
